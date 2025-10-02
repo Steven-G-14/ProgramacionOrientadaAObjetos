24d141f9580b921793674e0d16c9eb2a0132bf45
Steven-G-14 <gutuerrezangel38@gmail.com>
2025-10-02 17:36:02 -0500

    Add files via upload

---
+++ Corte1/Practicas Iniciales/Practica 2/Main.py
@@ -0,0 +1,13 @@
+import OperacionesAritmeticas
+
+obj = OperacionesAritmeticas.OperacionesBasicas()
+
+obj.setVal(20)
+obj.setVal2(11)
+
+obj.sumar(obj.getVal(),obj.getVal2())
+
+print(obj.getR())
+
+
+ 
---
+++ Corte1/Practicas Iniciales/Practica 2/OperacionesAritmeticas.py
@@ -0,0 +1,28 @@
+class OperacionesBasicas:
+    def __init__(self):
+     self.val= 0
+     self.val2 = 0
+     self.r = 0
+
+    def getVal(self):
+       return self.val
+    
+    def setVal(self, valor):
+       self.val=valor
+    
+    def getVal2(self):
+       return self.val2
+    
+    def setVal2(self, valor):
+       self.val2=valor
+     
+    def getR(self):
+       return self.r
+    
+    def setR(self, valor):
+       self.r=valor
+    
+    def sumar(self,a,b):
+       self.r = a + b
+
+
